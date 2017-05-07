f12437004795f7466c467e1e50a4abd04e756eca
Tzahi Ben Shitrit <freshybs@gmail.com>
2017-05-07 12:23:23 +0300

    fixed conflicts

--- GalaCasino/Base.cs
+++ GalaCasino/Base.cs
@@ -10,17 +10,10 @@ namespace GalaCasino
         [SetUp]
         public void Initialize()
         {
-<<<<<<< HEAD
-            driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            driver.Navigate().GoToUrl("https://www.galacasino.com/");
-            ExcelLib.PopulateInCollection(@"C:\Users\Tzahi.Ben\Documents\NUnit\Data.xlsx");
-=======
             PropertiesCollection.driver = new ChromeDriver();
             PropertiesCollection.driver.Manage().Window.Maximize();
             PropertiesCollection.driver.Navigate().GoToUrl("https://www.galacasino.com/");
             //ExcelLib.PopulateInCollection(@"C:\Users\Tzahi.Ben\Documents\NUnit\Data.xlsx");
->>>>>>> 79b42b8883ba8860e94f66042d6616da09b0afc0
         }
 
         [TearDown]
